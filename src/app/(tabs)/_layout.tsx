395a8142dfbf9f2c850e7090984decf2d32e9c14
Mark <markmccoid@gmail.com>
2026-02-22 23:35:54 -0500

    Reacticx added and BottomAccessory popup added

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,14 +1,17 @@
 import { useCachedBookSummary } from "@/hooks/abs-data-hooks";
 import { playerService, usePlaybackStore } from "@/player";
+import { useDeviceBooksActions } from "@/store/device-books-store";
 import { useThemeColors } from "@/theme/use-app-theme";
 import { Image } from "expo-image";
-import { Link, useRouter } from "expo-router";
+import { Link } from "expo-router";
 import { NativeTabs } from "expo-router/unstable-native-tabs";
 import { SymbolView } from "expo-symbols";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function TabLayout() {
-  const router = useRouter();
+  const playbackRate = usePlaybackStore((state) => state.rate);
+  const libraryItemId = usePlaybackStore((state) => state.libraryItemId);
+  const { setBookPlaybackRate } = useDeviceBooksActions();
   const currentLibraryItemId = usePlaybackStore((state) => state.libraryItemId);
   const currentBook = useCachedBookSummary(currentLibraryItemId ?? undefined);
   const themeColors = useThemeColors();
@@ -16,6 +19,12 @@ export default function TabLayout() {
   const isPlaying = playbackState === "playing";
 
   const hasLoadedBook = usePlaybackStore((s) => Boolean(s.libraryItemId) && s.queue.length > 0);
+  const handleSetRate = async (rate: number) => {
+    await playerService.setRate(rate);
+    if (libraryItemId) {
+      setBookPlaybackRate(libraryItemId, rate);
+    }
+  };
 
   const handleToggle = async () => {
     // if (isLoading) return;
@@ -56,45 +65,74 @@ export default function TabLayout() {
 
       {hasLoadedBook && (
         <NativeTabs.BottomAccessory>
-          <View
-            className="flex-row items-center justify-between h-full px-4 gap-2 bg-surface border-hairline border-gray-400 rounded-full"
-            // style={{ borderRadius:  }}
-          >
-            <Link href="/main-player" asChild>
-              <Pressable className="flex-row items-center justify-between flex-1 gap-2">
-                <Image
-                  source={{ uri: currentBook?.coverFull }}
-                  style={{
-                    width: 40,
-                    height: 40,
-                    borderRadius: 8,
-                    borderWidth: StyleSheet.hairlineWidth,
-                    borderColor: themeColors.border,
-                  }}
-                />
-                <View className="flex-col items-center flex-1 w-[100]">
-                  <Text style={{ fontSize: 12, color: themeColors.text }} numberOfLines={1}>
-                    {currentBook?.title}
-                  </Text>
-                  {/* <Text style={{ fontSize: 12, color: themeColors.textMuted }} numberOfLines={1}>
+          <View className="gap-1 flex-row items-center justify-between h-full px-4 bg-surface border-hairline border-gray-400 rounded-full">
+            <Link href="/main-player" className="flex-1 items-center flex-row  mt-[4]">
+              <Link.Trigger>
+                <View className="flex-row items-center h-full flex-1 ">
+                  <Image
+                    source={{ uri: currentBook?.coverFull }}
+                    style={{
+                      width: 40,
+                      height: 40,
+                      marginRight: 8,
+                      borderRadius: 8,
+
+                      borderWidth: StyleSheet.hairlineWidth,
+                      borderColor: themeColors.border,
+                    }}
+                  />
+
+                  <View className="flex-row flex-1 items-center h-full">
+                    <Text style={{ fontSize: 12, color: themeColors.text }} numberOfLines={1}>
+                      {currentBook?.title}
+                    </Text>
+                    {/* <Text style={{ fontSize: 12, color: themeColors.textMuted }} numberOfLines={1}>
                     by {currentBook?.duration}
                   </Text> */}
+                  </View>
                 </View>
-              </Pressable>
+              </Link.Trigger>
+              <Link.Menu>
+                <Link.MenuAction icon="book.closed.fill" onPress={() => playerService.stop()}>
+                  Close Book
+                </Link.MenuAction>
+                <Link.MenuAction icon="book.closed.fill" onPress={() => playerService.stop()}>
+                  Close Book
+                </Link.MenuAction>
+                <Link.Menu title="Speed" icon="hare.fill">
+                  <Link.MenuAction onPress={() => handleSetRate(0.75)} isOn={playbackRate === 0.75}>
+                    .75x
+                  </Link.MenuAction>
+                  <Link.MenuAction onPress={() => handleSetRate(1)} isOn={playbackRate === 1}>
+                    1x
+                  </Link.MenuAction>
+                  <Link.MenuAction onPress={() => handleSetRate(1.25)} isOn={playbackRate === 1.25}>
+                    1.25x
+                  </Link.MenuAction>
+                  <Link.MenuAction onPress={() => handleSetRate(1.5)} isOn={playbackRate === 1.5}>
+                    1.5x
+                  </Link.MenuAction>
+                  <Link.MenuAction onPress={() => handleSetRate(1.75)} isOn={playbackRate === 1.75}>
+                    1.75x
+                  </Link.MenuAction>
+                  <Link.MenuAction onPress={() => handleSetRate(2)} isOn={playbackRate === 2}>
+                    2x
+                  </Link.MenuAction>
+                </Link.Menu>
+              </Link.Menu>
             </Link>
-            {!isPlaying ? (
-              <Pressable onPress={handleToggle}>
+            {/* Play / Pause Icon */}
+            <Pressable
+              onPress={handleToggle}
+              className="h-full items-center flex-row w-8 justify-center"
+              hitSlop={10}
+            >
+              {!isPlaying ? (
                 <SymbolView name="play.fill" tintColor={themeColors.accent} />
-              </Pressable>
-            ) : (
-              <Pressable onPress={handleToggle}>
+              ) : (
                 <SymbolView name="pause.fill" tintColor={themeColors.accent} />
-              </Pressable>
-            )}
-            {/* <CustomTabTrigger name="library">
-            <NativeTabs.Trigger.Label>Library</NativeTabs.Trigger.Label>
-            <NativeTabs.Trigger.Icon sf="book.and.wrench" />
-          </CustomTabTrigger> */}
+              )}
+            </Pressable>
           </View>
         </NativeTabs.BottomAccessory>
       )}
